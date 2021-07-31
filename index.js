f596cfa2133e935b8cac553adb885bf0d7165d50
Erin Holmes <e.holmes09@yahoo.com>
2021-07-31 00:33:49 -0500

    add initial question with inquirer

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ db.connect(err => {
   if (err) throw err;
   console.log("Database connected.")
 });
+
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "pickOption",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Manager",
+        "Add Employee",
+        "Remvoe Employee",
+        "Update Employee Role",
+        "Update Employee Manager", 
+        "Quit"
+      ] 
+    }
+  ])
+}
+
+async function viewAllEmployees()
+
+promptUser()
+  .then(userPick => {
+    if (userPick === "View all Employees") {
+      viewAllEmployees();
+    }
+    console.log(userPick);
+  })
+
