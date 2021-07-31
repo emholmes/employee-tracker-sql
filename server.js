a54e44ea095895201806f0cceaacab7a15945de5
Erin Holmes <e.holmes09@yahoo.com>
2021-07-31 12:06:16 -0500

    update index.js to server.js, add/reorg inquirer list options

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const db = require("./config/connection");
 
 db.connect(err => {
   if (err) throw err;
-  console.log("Database connected.")
+  console.log("Database connected.");
+  promptUser();
 });
 
 
@@ -14,26 +15,34 @@ const promptUser = () => {
       name: "pickOption",
       message: "What would you like to do?",
       choices: [
+        "View All Departments",
+        "View All Roles",
         "View All Employees",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
         "View All Employees by Department",
         "View All Employees by Manager",
-        "Add Employee",
-        "Remvoe Employee",
-        "Update Employee Role",
-        "Update Employee Manager", 
+        "Remove Employee",
+        "Remove Department",
+        "Remove Role",
+        "View Total Utilized Budget of a Department",
         "Quit"
       ] 
     }
   ])
-}
-
-// async function viewAllEmployees()
-
-promptUser()
   .then(userPick => {
     if (userPick === "View all Employees") {
       viewAllEmployees();
     }
     console.log(userPick);
   })
+}
+
+// async function viewAllEmployees()
+
+
+  
 
