663aeb2e7c11bd9895c062340e46bab77c79c012
Erin Holmes <e.holmes09@yahoo.com>
2021-07-31 22:00:49 -0500

    update viewAllEmployees with manager_id join

--- server.js
+++ server.js
@@ -10,13 +10,15 @@ db.connect(err => {
 
 const viewAllEmployees = () => {
   const sql = `
-    SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary 
+    SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager  
     FROM employee
-    JOIN role ON employee.role_id = role.id
-    JOIN department ON role.department_id = department.id`;
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee AS manager ON employee.manager_id = manager.id`;
   db.query(sql, (err, result) => {
     if (err) throw err; 
-    console.log("------------------------------------------");
+    console.log(" ");
+    console.log(" ");
     console.table(result);
     promptUser();
   });
